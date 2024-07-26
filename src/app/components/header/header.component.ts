3200be8199a2a6e79f0f3858c83358012a6f61c4
vitorialuz229 <vitorialuz831@gmail.com>
2024-07-26 10:13:32 -0300

    :bug: fix: Correção na navegação do menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,30 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
+    CommonModule,
     NgOptimizedImage,
     MatIconModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  ngOnInit() {
+    const toggleButton = document.querySelector('.navbar-toggler');
+    const searchContainer = document.querySelector('.search-container');
+    const navbarBrand = document.querySelector('.navbar-brand');
+
+    if (toggleButton && searchContainer && navbarBrand) {
+      toggleButton.addEventListener('click', () => {
+        searchContainer.classList.toggle('show');
+        navbarBrand.classList.toggle('hide');
+      });
+    }
+  }
 }
