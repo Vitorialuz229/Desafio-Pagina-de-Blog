1e500c0f7d820ac72e9259bb5f42733705b83ddf
vitorialuz229 <vitorialuz831@gmail.com>
2024-07-29 22:20:54 -0300

    ":sparkles: feat: Implementando footer

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
